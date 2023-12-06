339ba73bb241b7c4e25e5cfad28714616a52c64e
yangkang <yangkang@skyworth.com>
2023-12-06 16:25:53 +0800

    add files

--- SDK/sdk_build.sh
+++ SDK/sdk_build.sh
@@ -14,12 +14,15 @@ export LANG=en_US.UTF-8
 
 export GIT_SSL_NO_VERIFY=1
 
-
-params_list=("$@")
-
-echo "params_list is: $params_list" 
-
-echo "MANIFEST is ${MANIFEST}"
+echo "MANIFEST=${MANIFEST}" 
+echo "BRANCH=${BRANCH}"
+echo "MACHINE_NAME=${MACHINE_NAME}" 
+echo "TARGET_IMAGE=${TARGET_IMAGE}" 
+echo "BUILD_PARAM=${BUILD_PARAM}" 
+echo "REPOURL=${REPOURL}" 
+echo "USE_SSTATE_CACHE=${USE_SSTATE_CACHE}" 
+echo "DISTRO_FEATURES=${DISTRO_FEATURES}" 
+echo "PATCHLIST=${PATCHLIST}" 
 
 # 异常处理函数
 function handle_error {
